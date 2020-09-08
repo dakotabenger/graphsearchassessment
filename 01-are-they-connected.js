67c6a3756a00e42fc2b0c6002e14511b522d7903
Dakota Benger <dakota@bengerlaw.com>
2020-09-08 10:15:27 -0700

    Finished

--- 01-are-they-connected.js
+++ 01-are-they-connected.js
@@ -20,16 +20,48 @@
 
 // Traverse the graph in Breadth-First Order
 function areTheyConnected(adjacencyList, startName, endName) {
-  throw new Error('Replace this with your implementation');
+  let queue = [startName];
+  let result = new Set();
+  let visited = [];
+  let currentNode;
+
+  if (startName === endName){
+    result.add(startName);
+    return result;
+  }
+  while (queue.length) {
+    currentNode = queue.shift();
+    visited.push(currentNode);
+    for (let i = 0; i < adjacencyList[currentNode].length; i++) {
+
+      let neighbor = adjacencyList[currentNode][i]  
+      console.log(neighbor === endName)
+      if (neighbor === endName) {
+        visited.push(neighbor);
+        for (let i = 0; i < visited.length; i++) {
+          result.add(visited[i]);
+
+        }
+        return result;
+      } else if (!visited.includes(neighbor)) {
+        visited.push(neighbor);
+        queue.push(neighbor);
+      
+      }
+
+
+    }
+    
+  }
+  
+    return null;
 }
-
-
 
 /*******************************************************************************
  * Do not change the code after this line.
  */
 try {
   exports.areTheyConnected = areTheyConnected;
-} catch(e) {
+} catch (e) {
   module.exports = null;
 }
